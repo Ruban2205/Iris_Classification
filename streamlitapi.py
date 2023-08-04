7369ff8e58cb34ec8c0122efcca4b0fb3463c590
Ruban Gino Singh <rubangino22@gmail.com>
2023-08-04 19:09:15 +0530

    COMPLETE | Streamlit demo

--- streamlitapi.py
+++ streamlitapi.py
@@ -2,11 +2,22 @@ import pickle
 import streamlit as st
 
 scaler_file = pickle.load(open('scaler.pkl', 'rb')) 
-model_file = pickle.load(open('model.pkl'))
+model_file = pickle.load(open('model.pkl', 'rb'))
 
 def main(): 
     st.title("Iris Classification - rubangino.in")
 
+    # Input Variables
+    sepalLength = st.text_input('Enter the Sepal Length (Cm)')
+    sepalWidth = st.text_input('Enter the Sepal Width in (Cm)')
+    petalLength = st.text_input('Enter the Petal Length in (Cm)')
+    petalWidth = st.text_input('Enter the Petal Width in (Cm)')
+
+    # Button to predict
+    if st.button('Predict'):
+        make_prediction = model_file.predict([[sepalLength, sepalWidth, petalLength, petalWidth]])
+        st.success(make_prediction)
     
 if __name__ == '__main__':
     main()
+
