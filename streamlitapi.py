d350073479130d540d224f9dcb363d52a9ee531f
Ruban Gino Singh <rubangino22@gmail.com>
2023-09-11 15:28:08 +0530

    Final Commit

--- streamlitapi.py
+++ streamlitapi.py
@@ -11,13 +11,20 @@ def pred_output(user_input):
     return ypred[0]
 
 def main(): 
-    st.title("Iris Classification - rubangino.in")
+    st.title('Iris Classification')
+    st.text('Developed by - www.rubangino.in')
+
+    st.divider()
 
     # Input Variables
     sepalLength = st.text_input('Enter the Sepal Length (Cm)')
     sepalWidth = st.text_input('Enter the Sepal Width in (Cm)')
     petalLength = st.text_input('Enter the Petal Length in (Cm)')
     petalWidth = st.text_input('Enter the Petal Width in (Cm)')
+
+    if sepalLength.isalpha() or sepalWidth.isalpha() or petalLength.isalpha() or petalWidth.isalpha():
+        st.error("Input must be a Numeric!", icon='🚨')
+        st.error("It seems some given input was not a Numeric Value!!", icon='🤔')
 
     # Button to predict
     if st.button('Predict'):
